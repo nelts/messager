31b69e420dca1731fd6af8af7e92a2cf17a775a6
evio <evio@vip.qq.com>
2019-08-02 12:08:04 +0800

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
-import Base from './base';
+import Base, { ReplyData, MessageReceiveDataOptions, NewAgentMessageReceiveDataOptions } from './base';
 import Master from './master';
 import Worker from './worker';
 import Agent from './agent';
-export { Base, Master, Worker, Agent, };
+export { Base, Master, Worker, Agent, ReplyData, MessageReceiveDataOptions, NewAgentMessageReceiveDataOptions, };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Base from './base';
+import Base, { ReplyData, MessageReceiveDataOptions, NewAgentMessageReceiveDataOptions } from './base';
 import Master from './master';
 import Worker from './worker';
 import Agent from './agent';
@@ -8,4 +8,7 @@ export {
   Master,
   Worker,
   Agent,
+  ReplyData, 
+  MessageReceiveDataOptions, 
+  NewAgentMessageReceiveDataOptions,
 }
